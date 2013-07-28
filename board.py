bf8a8ec92b7c873194b77015c68838625e44b974
Brad Van Vugt <bvanvugt@gmail.com>
2013-07-28 13:20:27 -0700

    asdf

--- board.py
+++ board.py
@@ -1,9 +1,10 @@
 
 
 class Board():
-    PIECE_FOOD = 'food'
-    PIECE_SNAKE_BODY = 'body'
-    PIECE_SNAKE_HEAD = 'head'
+    STATE_EMTPY = 'empty'
+    STATE_FOOD = 'food'
+    STATE_BODY = 'body'
+    STATE_HEAD = 'head'
 
     def __init__(self, board_state):
         self.board_state = board_state
@@ -13,3 +14,24 @@ class Board():
 
     def get_height(self):
         return len(self.board_state)
+
+    def get_state(self, x, y):
+        """ snake|food|snake_head """
+        square = self.board_state[y][x]
+
+        if square.type == 'food':
+            return self.STATE_FOOD
+        if square.type == 'body':
+            return self.STATE_BODY
+        if square.type == 'head':
+            return self.STATE_HEAD
+        return self.STATE_EMTPY
+
+    def is_empty(self, x, y):
+        return self.get_state(x, y) == self.STATE_EMTPY
+
+    def is_food(self, x, y):
+        return self.get_state(x, y) == self.STATE_FOOD
+
+    def is_snake(self, x, y):
+        return (self.get_state(x, y) == self.STATE_BODY) or (self.get_state(x, y) == self.STATE_HEAD)
