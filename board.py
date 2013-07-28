d380f3ed86bcc79b11d8d90a7529b041194afddc
Brad Van Vugt <bvanvugt@gmail.com>
2013-07-28 13:14:49 -0700

    asdf

--- board.py
+++ board.py
@@ -1,6 +1,15 @@
 
 
 class Board():
+    PIECE_FOOD = 'food'
+    PIECE_SNAKE_BODY = 'body'
+    PIECE_SNAKE_HEAD = 'head'
 
-    def __init__(board_state):
+    def __init__(self, board_state):
         self.board_state = board_state
+
+    def get_width(self):
+        return len(self.board_state[0])
+
+    def get_height(self):
+        return len(self.board_state)
