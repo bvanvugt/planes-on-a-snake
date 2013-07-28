f67e23c191fcc202e06c326a9a2da9cec9a269df
Will Reiher <wreleven@gmail.com>
2013-07-28 16:51:30 -0700

    Distance Weight

--- reward.py
+++ reward.py
@@ -20,7 +20,7 @@ def calc_reward(board):
 				food.append((x,y))
 
 
-	print 'Food: %s' % food
+	print 'All Food: %s' % food
 
 
 	# ITERATE OVER FOOD ON BOARD
@@ -29,13 +29,27 @@ def calc_reward(board):
 		distances[(x,y)]  = board.get_distance_to_point(x,y)
 
 	# ORDERED BY REGION AND DISTANCE
-	regions_ordered 	= list(sorted(regions, key=regions.__getitem__))
-	distances_orderd 	= list(sorted(distances, key=distances.__getitem__))
+	#regions_ordered 	= list(sorted(regions, key=regions.__getitem__))
+	distances_orderd 	= list(sorted(distances, key=distances.__getitem__, reverse=True))
+
+
 
 	#print regions_ordered
 	#print distances_orderd
+
 	if food:
+		for index, point in enumerate(distances_orderd, start=1):
+			regions[board.get_direction_to_point(point[0], point[1])] += index*len(food)
+
+		regions_ordered = list(sorted(regions, key=regions.__getitem__))
+
+		print 'Internal Weight: %s' % regions
+
 		for index, direction in enumerate(regions_ordered, start=1):
 			reward[direction] = index*25
+
+
+
+
 
 	return reward
