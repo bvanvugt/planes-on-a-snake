ba0aa8d5bb5b11412622f3c5ea2aee28b6e7ca9a
unknown <briancdang@gmail.com>
2013-07-28 15:11:08 -0700

    risk - last move changes

--- risk.py
+++ risk.py
@@ -1,4 +1,4 @@
-def calc_risk(self, board):
+def calc_risk(board, last_move):
     risk = {'n': 0,
             'e': 0,
             's': 0,
@@ -30,22 +30,15 @@ def calc_risk(self, board):
         risk['w'] = 100
     
     # Don't hit self, but do it as a last resort
-    last_move = board.get_head().get_last_move()
-    
-    if last_move == 'e':
-        risk['w'] = 99
-    elif last_move == 'w':
-        risk['e'] = 99
-    elif last_move == 'n':
-        risk['s'] = 99
-    elif last_move == 's':
-        risk['n'] = 99
-        
-    # Calculate risk with proximity to walls
-    
-    # Calculate risk with proxomity to snake bodies
-    
-    # Calculate risk with proximity to snake heads
+    if len(last_move) > 0:        
+        if last_move == 'e':
+            risk['w'] = 99
+        elif last_move == 'w':
+            risk['e'] = 99
+        elif last_move == 'n':
+            risk['s'] = 99
+        elif last_move == 's':
+            risk['n'] = 99
     
     # Calculate risk of going in the direction of clusters of snakes
     
@@ -55,5 +48,47 @@ def calc_risk(self, board):
 
 # Pick the max of current risk and desired risk
 def _max_risk(risk, direction, desired_risk):
+    max_risk = 98 # stay under suicide risk
     if risk[direction] < desired_risk:
-        risk[direction] = desired_risk
+        if desired_risk > max_risk:
+            risk[direction] = 98
+        else:
+            risk[direction] = desired_risk
+
+# Calculate proxomity risk
+def _proximity_risk(board, risk, our_x, our_y):
+    prox_risk = {'n': 0,
+                 'e': 0,
+                 's': 0,
+                 'w': 0}
+    max_dist = 4
+    for d in range(1, max_dist + 1):
+        # Start east
+        xd = d
+        yd = 0
+        dir = 'e'
+        risk_factor = max_dist + 1 # start at max + 1, go down to 1
+        cur_x = our_x + xd
+        cur_y = our_y + yd
+        
+        # Calculate risk with proximity to walls
+        if board.is_wall(cur_x, cur_y):
+            _max_risk(prox_risk, dir, prox_risk[dir] + risk_factor * 1)
+    
+        # Calculate risk with proximity to snake bodies, including own snake body
+        if board.get_state(cur_x, cur_y) == STATE_BODY:
+            _max_risk(prox_risk, dir, prox_risk[dir] + risk_factor * 2)
+    
+        # Calculate risk with proximity to snake heads
+        if board.get_state(cur_x, cur_y) == STATE_HEAD:
+            _max_risk(prox_risk, dir, prox_risk[dir] + risk_factor * 3)
+            
+        xd -= 1
+        yd += 1
+
+    _max_risk(risk, 'n', prox_risk['n'])
+    _max_risk(risk, 'e', prox_risk['e'])
+    _max_risk(risk, 's', prox_risk['s'])
+    _max_risk(risk, 'w', prox_risk['w'])
+    
+def _calc_prox_risk(board, ):
