b5bcdf2cfa106c069137520810fbd3fc602cf525
Will Reiher <wreleven@gmail.com>
2013-07-28 18:30:09 -0700

    Risks and Rewards

--- reward.py
+++ reward.py
@@ -1,4 +1,5 @@
 def calc_reward(board):
+
 	food 		= []
 
 	reward 		= {	'n': 	0,
@@ -28,10 +29,10 @@ def calc_reward(board):
 		for x, y in food:
 
 			# BASIC WEIGHTING TO DIRECTIONS
-			regions[board.get_direction_to_point(x,y)] 		+= 1
+			regions[board.get_direction_to_point(x,y)] += 1
 
 			#BUILD LIST OF FOOD POINTS AND DISTANCE
-			distances[(x,y)]  								= board.get_distance_to_point(x,y)
+			distances[(x,y)] = board.get_distance_to_point(x,y)
 
 		# ORDER THE DISTANCES FROM HEAD
 		# REVERSED SO BIGGER INDEX IS CLOSER
@@ -52,3 +53,56 @@ def calc_reward(board):
 			reward[direction] = index*13
 
 	return reward
+
+
+def calc_risks(board, item='food'):
+
+	types 		= ['food', 'snake']
+	items 		= []
+	risk 		= {	'n': 	0,
+					'e': 	0,
+					'w': 	0,
+					's': 	0}
+
+
+	for x in range(board.get_width()):
+		for y in range(board.get_height()):
+			if item == 'food' and board.is_food(x,y):
+				items.append((x,y))
+			elif item == 'snake' and board.is_snake(x,y):
+				items.append((x,y))
+
+
+	print '%d %s(s): %s' % (len(items), item, items)
+
+	if items:
+
+		# ITERATE OVER FOOD ON BOARD
+		for x, y in items:
+
+			# BASIC WEIGHTING TO DIRECTIONS
+			regions[board.get_direction_to_point(x,y)] += 1
+
+			#BUILD LIST OF FOOD POINTS AND DISTANCE
+			distances[(x,y)] = board.get_distance_to_point(x,y)
+
+		# ORDER THE DISTANCES FROM HEAD
+		# REVERSED SO BIGGER INDEX IS CLOSER
+		distances_orderd 	= list(sorted(distances, key=distances.__getitem__, reverse=True))
+
+
+		# HEAVY WEIGHTING INDEX ADDED TO DIRECTION
+		for index, point in enumerate(distances_orderd, start=1):
+			regions[board.get_direction_to_point(point[0], point[1])] += index
+
+		#ORDER BY WEIGHT
+		regions_ordered = list(sorted(regions, key=regions.__getitem__))
+
+		print 'Internal Weight: %s' % regions
+
+		#EXTERNAL WEIGHTING IS SIMPLIFIED FOR NOW
+		for index, direction in enumerate(regions_ordered, start=1):
+			risk[direction] = index*25
+
+	return risk
+
