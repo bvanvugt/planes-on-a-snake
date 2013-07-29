25d0962765c027dc6c25b154520363aadaf72d2b
Brad Van Vugt <bvanvugt@gmail.com>
2013-07-28 18:10:59 -0700

    asdf

--- risk.py
+++ risk.py
@@ -16,35 +16,37 @@ def calc_risk(board, last_move):
     # Easy risk calculation for bad moves
     # Don't hit walls
     if our_y == 0:
-        risk['n'] = 100
-    elif our_y == board.get_height():
-        risk['s'] = 100
+        risk['n'] += 100
+    elif our_y == (board.get_height() - 1):
+        risk['s'] += 100
 
     if our_x == 0:
-        risk['w'] = 100
-    elif our_x == board.get_width():
-        risk['e'] = 100
+        risk['w'] += 100
+    elif our_x == (board.get_width() - 1):
+        risk['e'] += 100
 
     # Don't hit other snakes (doesn't matter if self for now)
     if board.is_snake(our_x, our_y - 1):
-        risk['n'] = 100
+        risk['n'] += 100
     if board.is_snake(our_x + 1, our_y):
-        risk['e'] = 100
+        risk['e'] += 100
     if board.is_snake(our_x, our_y + 1):
-        risk['s'] = 100
+        risk['s'] += 100
     if board.is_snake(our_x - 1, our_y):
-        risk['w'] = 100
+        risk['w'] += 100
 
     # Don't hit self, but do it as a last resort
+    """
     if len(last_move) > 0:
         if last_move == 'e':
-            risk['w'] = 99
+            risk['w'] += 99
         elif last_move == 'w':
-            risk['e'] = 99
+            risk['e'] += 99
         elif last_move == 'n':
-            risk['s'] = 99
+            risk['s'] += 99
         elif last_move == 's':
-            risk['n'] = 99
+            risk['n'] += 99
+    """
 
     pp = pprint.PrettyPrinter(indent=4)
 
@@ -52,13 +54,18 @@ def calc_risk(board, last_move):
     pp.pprint(risk)
 
     # Calculate proximity based risk
-    _proximity_risk(board, risk, our_x, our_y)
+    prox_risk = _proximity_risk(board, risk, our_x, our_y)
+    for m, score in prox_risk.iteritems():
+        risk[m] += score
 
     print "After proximity risk calc:"
     pp.pprint(risk)
 
     # Calculate risk of going in the direction of other snakes, considering snake length
-    _calc_snake_risk(board, risk, our_x, our_y)
+    snake_risk = _calc_snake_risk(board, risk, our_x, our_y)
+    for m, score in snake_risk.iteritems():
+        risk[m] += score
+
     print "After quadrant risk calc:"
     pp.pprint(risk)
 
