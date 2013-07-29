e9742df697a7b1e9976c3fe923c1a4c6324cc7b2
Will Reiher <wreleven@gmail.com>
2013-07-28 17:01:15 -0700

    Weighted Food

--- reward.py
+++ reward.py
@@ -19,37 +19,36 @@ def calc_reward(board):
 			if board.is_food(x,y):
 				food.append((x,y))
 
+	print '%d Food(s): %s' % (len(food), food)
 
-	print 'All Food: %s' % food
-
-
-	# ITERATE OVER FOOD ON BOARD
-	for x, y in food:
-		regions[board.get_direction_to_point(x,y)] += 1
-		distances[(x,y)]  = board.get_distance_to_point(x,y)
-
-	# ORDERED BY REGION AND DISTANCE
-	#regions_ordered 	= list(sorted(regions, key=regions.__getitem__))
-	distances_orderd 	= list(sorted(distances, key=distances.__getitem__, reverse=True))
-
-
-
-	#print regions_ordered
-	#print distances_orderd
-
+	#  THERE IS FOOD IN PLAY
 	if food:
-		for index, point in enumerate(distances_orderd, start=1):
-			regions[board.get_direction_to_point(point[0], point[1])] += index*len(food)
 
+		# ITERATE OVER FOOD ON BOARD
+		for x, y in food:
+
+			# BASIC WEIGHTING TO DIRECTIONS
+			regions[board.get_direction_to_point(x,y)] 		+= 1
+
+			#BUILD LIST OF FOOD POINTS AND DISTANCE
+			distances[(x,y)]  								= board.get_distance_to_point(x,y)
+
+		# ORDER THE DISTANCES FROM HEAD
+		# REVERSED SO BIGGER INDEX IS CLOSER
+		distances_orderd 	= list(sorted(distances, key=distances.__getitem__, reverse=True))
+
+
+		# HEAVY WEIGHTING INDEX ADDED TO DIRECTION
+		for index, point in enumerate(distances_orderd, start=1):
+			regions[board.get_direction_to_point(point[0], point[1])] += index
+
+		#ORDER BY WEIGHT
 		regions_ordered = list(sorted(regions, key=regions.__getitem__))
 
 		print 'Internal Weight: %s' % regions
 
+		#EXTERNAL WEIGHTING IS SIMPLIFIED FOR NOW
 		for index, direction in enumerate(regions_ordered, start=1):
 			reward[direction] = index*25
-
-
-
-
 
 	return reward
